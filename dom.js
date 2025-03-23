e15e0f9e0ca763327dc9abc9b5d8b4247cc002fe
George-25 <georgemallory26@gmail.com>
2025-03-23 15:29:29 -0400

    first commit

---
+++ dom.js
@@ -0,0 +1,59 @@
+// Arkar Kaung Myat
+// 11:29 PM
+// - show cross, on click for every blocks
+// - every square must have click event listener
+//let player = "x";
+
+// const change = document.querySelectorAll(".sqr");
+
+// // console.log(change[0].classList.);
+
+// change.forEach((ele) => {
+//   // ele.style.backgroungColor = "purple";
+
+//   ele.addEventListener("click", () => {
+
+//     if (ele.classList.contains('color-change')) {
+//       ele.classList.remove("color-change");
+//     } else {
+//       ele.classList.add('color-change')
+//     }
+//   })
+// }
+// )
+
+
+
+
+//   if (ele.body.style.color == "purple") {
+//     ele.body.style.color = "black";
+//   } else {
+//     ele.body.style.color = "purple";
+//   }
+// })
+
+//  
+// }
+// document.querySelector(".decor").addEventListener('click', changeColor);
+
+// function changeColor() {
+//   console.log(this.style.color);
+//   if (this.style.color == "blue") {
+//     this.style.color = "black"
+//   } else {
+//     this.style.color = "blue";
+
+//   }
+
+// }
+
+
+// const update = document.querySelector("decor");
+// update.addEventListener('click', () => {
+//   document.style.color = "green";
+// })
+
+
+
+
+
